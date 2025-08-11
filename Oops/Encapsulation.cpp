6ee76e0ca0958d8f596389979ef6913741863845
Divyansh Rajpoot <157517609+RajpootDivyansh@users.noreply.github.com>
2025-08-11 19:01:40 +0530

    Add files via upload

---
+++ Oops/Encapsulation.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Teacher{
+private:
+    double salary;
+
+public:
+    //properties
+    string name;
+    string dept;
+    string subject;
+    
+
+    //methods/member functions
+    void changeDept(string newDept){
+        dept=newDept;
+    }
+    //setter
+    void setSalary(double s){
+        salary =s;
+    }
+    //getter
+    double getSalary(){
+        return salary;
+    }
+
+};
+
+class Account{
+    private:
+        double balance;
+        string password;
+    public:
+        string accountId;
+        string username;
+    
+
+}
+
+int main(){
+    Teacher t1;
+ 
+    t1.name ="Divyansh";
+    t1.subject ="C++";
+    t1.dept="Computer Science";
+    t1.setSalary(25000);
+    cout<<t1.name<<endl;
+    cout<<t1.getSalary()<<endl;
+
+    return 0;
+}
