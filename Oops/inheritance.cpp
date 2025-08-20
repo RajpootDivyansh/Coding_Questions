16aceff4b5337c373b72494d2b14057f871e57ec
Divyansh Rajpoot <157517609+RajpootDivyansh@users.noreply.github.com>
2025-08-21 04:56:22 +0530

    Add files via upload

---
+++ Oops/inheritance.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Person{
+    public:
+    string name;
+    int age;
+
+    Person(string name,int age){
+        this->name=name;
+        this->age=age;
+    }
+    Person(){
+        cout<<"Parent"<<endl;
+    }
+};
+
+class Student:public Person{
+    //name,age ,rollno
+    public:
+    int rollno;
+
+
+
+    Student(string name,int age,int rollno):Person(name,age){
+        cout<<"Child"<<endl;
+    }
+
+    void getInfo(){
+        cout<<"name:"<<name<<endl;
+        cout<<"age:"<<age<<endl;
+        cout<<"rollno:"<<rollno<<endl;
+    }
+}
+
+
+
+int main(){
+    Student s1;
+    s1.name="Divyansh Rajpoot";
+    s1.age=21;
+    s1.rollno=2221;
+
+    s1.getInfo();
+    return 0;
+}
